1dbd1af430a6508038f4454eb3e8fa35a1179cc4
mignonnesaurus <elifloresch@gmail.com>
2017-08-24 12:56:12 +0200

    Complete about_triangle_project_2 and about_exceptions

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError unless a + b > c && a + c > b && b + c > a # Triangle Inequality Theorem
   return :equilateral if a == b && b == c
   return :isosceles if a == b || b == c || a == c
   :scalene
